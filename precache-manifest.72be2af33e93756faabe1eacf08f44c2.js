65ed9d6067fbe0fe158858b5c78a39afdb3231df
ishaypoz <ishay824@gmail.com>
2020-08-15 17:02:54 +0300

    Updates

---
+++ precache-manifest.72be2af33e93756faabe1eacf08f44c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a010e8e50a8db945bb0b738f854c5b3",
+    "url": "/Song-Choose/index.html"
+  },
+  {
+    "revision": "33e38b0548b60395d65b",
+    "url": "/Song-Choose/static/js/2.2fde1707.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Song-Choose/static/js/2.2fde1707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd07eb218c5d27273f9",
+    "url": "/Song-Choose/static/js/main.ccb27404.chunk.js"
+  },
+  {
+    "revision": "3953febb0b863efac888",
+    "url": "/Song-Choose/static/js/runtime-main.e6e494c3.js"
+  }
+]);
